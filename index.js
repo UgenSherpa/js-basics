9417d2e9c14a40e0cfec98e8a067fb62b8e4db95
ugensherpa <ugensherpa1997@gmail.com>
2024-01-23 20:14:22 +0545

    assignment

--- index.js
+++ index.js
@@ -1,1 +1,56 @@
-this
+console.log("helloworld");//prints the message
+//variables
+
+var broadwayTeacher = "raktim";
+console.log(broadwayTeacher); //always use camel case for joining words
+
+let teacher="raktim";
+teacher="sita";
+console.log(teacher);
+
+const username = "ugen";
+
+console.log(username);
+
+const table = "redtable";
+console.log(table);
+
+const fan = "bigfan";
+console.log(fan);
+/*
+ table 
+ fanchair 
+ funiture 
+ pen
+ (multi line comment)
+ */
+
+
+let sport ="football";
+sport="basketball";
+console.log(sport);
+
+let musicGenre = "r&b";
+musicGenre = "hiphop";
+console.log(musicGenre);
+
+let danceType ="bboy";
+danceType="salsa";
+console.log(danceType);
+
+let subject = "maths";
+subject = "computer";
+console.log(subject)
+
+
+
+
+
+
+
+
+
+
+9
+
+
